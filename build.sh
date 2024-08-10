01e5145814bdc5c39d27ddd6e0090e622345e5b6
reujab <reujab@gmail.com>
2024-08-10 18:20:07 -0400

    archive build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -eu
 set -o pipefail
-shopt -s globstar extglob # nullglob
+shopt -s globstar nullglob
 
 help() {
 	echo "Usage: $0 [options]"
 	echo
 	echo "      --deb-http-root=/var/www/html"
-	echo "  -x, --debug"
 	echo "      --force-template=<template>"
+	echo "      --get-metadata"
 	echo "  -h, --help"
 	echo "  -d, --make-deb"
 	echo "      --minify-args=<args>"
@@ -17,71 +17,41 @@ help() {
 	echo "  -w, --watch"
 }
 
-_minify() { if ((minify)); then minify "$@" "${minify_args[@]}"; else cat; fi }
+_minify() { if ((minify)); then minify "$@" "${minify_args[@]}"; else cat; fi; }
 
 rename() {
 	local dir base renamed out
 	dir=$(dirname "$1")
-	base=$(basename "$1" "$2")
-	renamed="$dir/$base$3"
-	out=${renamed/src/dist}
+	base=$(basename "$1" "${2:-}")
+	renamed="$dir/$base${3:-}"
+	out=$(sed -E 's/\/[0-9]{6}-/\//g' <<< "${renamed/src/dist}")
 	mkdir -p "$(dirname "$out")"
 	echo "$out"
 }
 
 init() {
 	start=$EPOCHREALTIME
-	pandoc_args+=(-C --toc=false --table-of-contents=false --wrap=none)
+	prev_wd=$PWD
+	cd "$(dirname "$0")"
+
+	# get-metadata
+	# exit 0
+	# metadata=$(get-metadata)
+	get-metadata > /tmp/meta.json
+
+	process-filters
+	rm -r dist
+	mkdir dist
+}
+
+process-filters() {
 	for filter in filters/*.lua; do
 		pandoc_args+=(--lua-filter="$filter")
 	done
-
-	prev_wd=$PWD
-	cd "$(dirname "$0")"
-	website=
-
-	rsync -a --delete --exclude={"*.bib","*.md","*.sass"} src/ static/ dist/
-}
-
-build-markdown() {
-	local args template out
-	args=("${pandoc_args[@]}" --resource-path="$(dirname "$1")" "$1")
-	template=$(get-template "$1")
-	args+=(--template="templates/$template")
-
-	out=$(rename "$1" .md .html)
-	pandoc "${args[@]}" |
-	sed -r 's/<a href="#cb[0-9]+-[0-9]+" aria-hidden="true" tabindex="-1"><\/a>| id="cb[0-9]+(-[0-9]+)?"//g' |
-	_minify --type html > "$out"
-	if ((debug)); then wait; fi
-}
-
-get-template() {
-	if [[ $force_template ]]; then echo "$force_template"; return; fi
-
-	local file base
-	file=$(readlink -f "$1")
-	while [[ $file != / ]]; do
-		base=$(basename "$file" .md)
-		if [[ -f templates/$base.html ]]; then
-			echo "$base"
-			return
-		fi
-
-		file=$(dirname "$file")
-	done
-	echo default
-}
-
-build-sass() {
-	local out
-	out=$(rename "$1" .sass .css)
-	sassc -a "$1" | _minify --type css > "$out"
-	if ((debug)); then wait; fi
 }
 
 build-highlighting-css() {
-	get-highlighting-css | _minify --type css > "dist/highlighting.css"
+	get-highlighting-css | _minify --type css > dist/highlighting.css
 }
 
 get-highlighting-css() {
@@ -95,6 +65,118 @@ get-highlighting-css() {
 		grep '^code span\.' | sed 's/^code span//'
 		echo "}"
 	fi
+}
+
+traverse() {
+	local out
+	for file in "$1"/*; do
+		if [[ -d $file ]]; then
+			traverse "$file"
+		elif [[ $file = *.md ]]; then
+			build-markdown "$file" & jobs+=($!)
+		elif [[ $file = *.sass ]]; then
+			build-sass "$file" & jobs+=($!)
+		elif [[ $file =~ \.(bib)$ ]]; then
+			:
+		else
+			out=$(rename "$file")
+			cp -a "$file" "$out"
+		fi
+	done
+}
+
+get-metadata() {
+	echo "{"
+	get-metadata-for-dir src 1
+	echo "}"
+}
+
+get-metadata-for-dir() {
+	local first=1 key files file dir href
+	key=$(get-key "$1")
+	if ((!$2)); then echo ,; fi
+	echo "\"$key\":{"
+	files=("$1"/*)
+	for i in "${!files[@]}"; do
+		file="${files[$i]}"
+		if [[ -d $file ]]; then
+			get-metadata-for-dir "$file" "$first"
+			first=0
+		elif [[ $file = *.md ]]; then
+			if ((!first)); then echo ,; fi
+			first=0
+
+			key=$(get-key "$file")
+			if [[ $file = */index.md ]]; then
+				dir=$(rename "$1")
+				href=$(basename "$dir")/
+			else
+				# FIXME
+				href=$(rename "$1" .md .html)
+			fi
+
+			echo "\"$key\":"
+			sed -n '/^---$/,/^---$/ { /^---$/d; p }' "$file" |
+			yaml2json | head -c-1
+			echo ",\"href\":\"$href\""
+			echo ",\"preview\":"
+head -n20 "$file" | pandoc | tr -d '\n' >&2
+			jq -Rn --arg preview "$(head -n20 "$file" | pandoc --wrap=none | tr -d '\n')" '$preview'
+			echo "}"
+		fi
+	done
+	echo "}"
+}
+
+get-key() {
+	local out base
+	out=$(rename "$1")
+	out=${out/dist/src}
+	base=$(basename "$out")
+	echo "${base//./_}"
+	# basename "$out" | sed 's/[-.]//g'
+}
+
+build-markdown() {
+	local args template out
+	args=("${pandoc_args[@]}" --resource-path="$(dirname "$1")" "$1")
+	template=$(get-template "$1")
+	args+=(--template="templates/$template")
+
+	out=$(rename "$1" .md .html)
+	# pandoc "${args[@]}" --metadata-file=<(echo "$metadata") |
+	pandoc "${args[@]}" --metadata-file=/tmp/meta.json |
+	# This removes unused code block line number references.
+	sed -r 's/<a href="#cb[0-9]+-[0-9]+" aria-hidden="true" tabindex="-1"><\/a>| id="cb[0-9]+(-[0-9]+)?"//g' |
+	# htmlq -r ".preview br" |
+	_minify --type html > "$out"
+}
+
+get-template() {
+	if [[ $force_template ]]; then echo "$force_template"; return; fi
+
+	local file base
+	file=$1
+	while [[ $file != . ]]; do
+		base=$(basename "$file" .md)
+		dir=$(dirname "$file")
+		dir_base=$(basename "$dir")
+		paths=("$dir_base.$base" "$base")
+		for path in "${paths[@]}"; do
+			if [[ -f templates/$path.html ]]; then
+				echo "$path.html"
+				return
+			fi
+		done
+		file=$(dirname "$file")
+	done
+	echo default.html5
+}
+
+build-sass() {
+	local out
+	out=$(rename "$1" .sass .css)
+	sassc -a "$1" | _minify --type css > "$out"
 }
 
 make-deb() {
@@ -123,36 +205,33 @@ watch() {
 
 	local pid=
 	while read -r; do
-		if [[ $pid && -d /proc/$pid ]]; then
-			if [[ $(ps -ho comm --ppid $pid) != sleep ]]; then
-				echo Aborting
-			fi
-			kill "$pid"
-		fi
+		if [[ $pid && -d /proc/$pid ]]; then kill $pid; fi
 		(
-			sleep 0.5
+			# Typically, when a file is modified, several events are triggered.
+			# This prevents a bunch of "Recompiling..." messages for a single change.
+			sleep 0.1
 			echo Recompiling...
-			$0 "$@" || {
-				echo Exit code $? >&2
-			}
+			exec $0 "$@"
 		) & pid=$!
 	done < <(inotifywait -mr -e{modify,close_write,move{,_self},delete{,_self}} filters src static templates themes)
 }
 
 cleanup() {
 	code=$?
-	pkill --parent $$ || true
+	local jobs
+	jobs=$(jobs > /dev/null; jobs -p)
+	if [[ $jobs ]]; then kill $jobs; fi
 	exit $code
 }
 trap cleanup EXIT
 
 dark_theme=1
-debug=0
 force_template=
 make_deb=0
 minify=1
 minify_args=()
-opts=$(getopt -n "$0" -o xhdw -l deb-http-root:,debug,help,force-template:,make-deb,minify-args:,no-dark-theme,no-minify,pandoc-args:,watch -- "$@") || {
+pandoc_args=(-C --toc=false --table-of-contents=false --wrap=none)
+opts=$(getopt -n "$0" -o xhdw -l deb-http-root:,get-metadata,help,force-template:,make-deb,minify-args:,no-dark-theme,no-minify,pandoc-args:,watch -- "$@") || {
 	echo
 	help
 	exit 1
@@ -164,13 +243,14 @@ while (($#)); do
 	 	deb_http_root=$2
 		shift
 		;;
-	-x|--debug)
-		set -x
-		debug=1
-		;;
 	--force-template)
 	 	force_template=$2
 		shift
+		;;
+	--get-metadata)
+		get-metadata
+		get-metadata | jq
+		exit
 		;;
 	-h|--help)
 		help
@@ -196,7 +276,7 @@ while (($#)); do
 		;;
 	-w|--watch)
 		shift
-		watch "$@"
+		watch --no-minify "$@"
 		exit
 		;;
 	--);;
@@ -211,19 +291,11 @@ done
 
 init
 
-for file in src/**/*.md; do
-	build-markdown "$file" & jobs+=($!)
-	if ((debug)); then wait; fi
-done
+build-highlighting-css & jobs+=($!)
 
-for file in **/*.sass; do
-	build-sass "$file" & jobs+=($!)
-	if ((debug)); then wait; fi
-done
+traverse src
 
-build-highlighting-css
-
-if ((!debug)); then wait "${jobs[@]}"; fi
+for job in "${jobs[@]}"; do wait "$job"; done
 if ((make_deb)); then make-deb; fi
 
 echo "Done in $(bc <<< "scale=2; ($EPOCHREALTIME - $start)/1")s"
