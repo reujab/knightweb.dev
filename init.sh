be0a27b8a1ba7f3da1ea030dea253141ba08358f
reujab <reujab@gmail.com>
2024-08-11 18:02:40 -0400

    fixed scripts

--- init.sh
+++ init.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -eux
 set -o pipefail
 
+replace() { sed -i "s/^compile_sass = $1$/compile_sass = $2/" config.toml; }
+
 cd "$(dirname "$0")"
-sed -i 's/^compile_sass = true$/compile_sass = false/' config.toml
+rm -f static/highlighting*.css
+replace true false
 zola build
 for file in static/highlighting*.css; do minify "$file" -o "$file"; done
-sed -i 's/^compile_sass = false$/compile_sass = true/' config.toml
+replace false true
 zola build
--- make_deb.sh
+++ make_deb.sh
@@ -4,10 +4,12 @@ set -o pipefail
 prev_wd=$PWD
 cd "$(dirname "$0")"
 website=$(basename "$PWD")
+deb=$(mktemp -d)
+public=$deb/var/www/html
 tmp=$(mktemp -d)
-mkdir "$tmp/DEBIAN"
-mkdir -p "$tmp/var/www"
-cat > "$tmp/DEBIAN/control" << EOF
+trap 'rm -rf "$deb" "$tmp"' EXIT
+mkdir "$deb/DEBIAN"
+cat > "$deb/DEBIAN/control" << EOF
 Package: $website
 Version: $(date +%F)
 Architecture: all
@@ -15,8 +17,10 @@ Maintainer: --
 Description: $website
 EOF
 
-zola build
-rm public/404.html
-minify public/atom.xml -o public/atom.xml
-cp -a public "$tmp/var/www/html"
-dpkg-deb --build "$tmp" "$prev_wd"
+zola build -fu "https://$website" -o "$tmp"
+zola build -o "$public"
+rm "$public"/{404.html,highlighting.css}
+mv "$tmp"/*.xml "$public"
+for file in "$public"/*.{css,xml}; do minify "$file" -o "$file"; done
+echo "Sitemap: https://$website/sitemap.xml" >> "$public/robots.txt"
+dpkg-deb --build "$deb" "$prev_wd"
